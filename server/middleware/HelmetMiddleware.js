4af58ed840fdac5eb5103ecb85c1eccd27afa6ac
Hemn-Raqib <hemnraqeeb02@gmail.com>
2025-02-14 00:58:50 +0300

    Update HelmetMiddleware.js

--- server/middleware/HelmetMiddleware.js
+++ server/middleware/HelmetMiddleware.js
@@ -4,7 +4,6 @@ import xss from 'xss-clean';
 const configureHelmet = (app) => {
   app.use(helmet());
 
-  // Custom Content Security Policy
   app.use(
     helmet.contentSecurityPolicy({
       directives: {
@@ -21,25 +20,18 @@ const configureHelmet = (app) => {
     })
   );
 
-  // Enable XSS filter
   app.use(helmet.xssFilter());
 
-  // Use xss-clean to sanitize input
   app.use(xss());
 
-  // Set strict transport security for 1 year
   app.use(helmet.hsts({ maxAge: 31536000, includeSubDomains: true, preload: true }));
 
-  // Prevent clickjacking
   app.use(helmet.frameguard({ action: 'deny' }));
 
-  // Hide X-Powered-By header
   app.use(helmet.hidePoweredBy());
 
-  // Disable MIME type sniffing
   app.use(helmet.noSniff());
 
-  // Set referrer policy
   app.use(helmet.referrerPolicy({ policy: 'strict-origin-when-cross-origin' }));
 };
 
